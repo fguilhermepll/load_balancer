73443350805bd7364b2c27f4c790644200530ebd
fguilhermepll <fguilhermepll@gmail.com>
2022-02-20 00:18:11 -0300

    functional loadbalancing

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import requests, random
+import yaml
 
 loadbalancer = Flask(__name__)
 
+def load_configuration(path):
+    with open(path) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    return config
+
+config = load_configuration('loadbalancer.yaml')
+
 @loadbalancer.route('/')
 def router():
-    return 'hello'
+    host_header = request.headers["Host"]
+    for entry in config["hosts"]:
+        if host_header == entry["host"]:
+            response = requests.get("http://{}".format(random.choice(entry["servers"])))
+            return response.content, response.status_code
+    
+    return "Not Found", 404
+
+@loadbalancer.route("/<path>")
+def path_router(path):
+    for entry in config["paths"]:
+        if ("/" + path) == entry["path"]:
+            response = requests.get("http://{}".format(random.choice(entry["servers"])))
+            return response.content, response.status_code
+
+    return "Not Found", 404
--- test_loadbalancer.py
+++ test_loadbalancer.py
@@ -6,15 +6,17 @@ def client():
     with loadbalancer.test_client() as client:
         yield client
 
-def test_host_routing_mango(client):
-    result = client.get('/', headers={'Host': 'www.mango.com'})
-    assert b'This is the mango application' in result.data
+def test_path_routing_mango(client):
+    result = client.get('/mango')
+    assert b'This is the mango application.' in result.data
 
-def test_host_routing_apple(client):
-    result = client.get('/', headers={'Host': 'www.apple.com'})
-    assert b'This is the apple application' in result.data
 
-def test_host_routing_notfound(client):
-    result = client.get('/', headers={'Host': 'www.notmango.com'})
+def test_path_routing_apple(client):
+    result = client.get('/apple')
+    assert b'This is the apple application.' in result.data
+
+
+def test_path_routing_notfound(client):
+    result = client.get('/notmango')
     assert b'Not Found' in result.data
     assert 404 == result.status_code
