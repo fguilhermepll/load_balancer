4719bc2f04606f315ee051a8bd26410478bd6eaf
fguilhermepll <fguilhermepll@gmail.com>
2022-02-19 23:35:22 -0300

    Innitial commit

---
+++ test_loadbalancer.py
@@ -0,0 +1,20 @@
+import pytest
+from loadbalancer import loadbalancer
+
+@pytest.fixture
+def client():
+    with loadbalancer.test_client() as client:
+        yield client
+
+def test_host_routing_mango(client):
+    result = client.get('/', headers={'Host': 'www.mango.com'})
+    assert b'This is the mango application' in result.data
+
+def test_host_routing_apple(client):
+    result = client.get('/', headers={'Host': 'www.apple.com'})
+    assert b'This is the apple application' in result.data
+
+def test_host_routing_notfound(client):
+    result = client.get('/', headers={'Host': 'www.notmango.com'})
+    assert b'Not Found' in result.data
+    assert 404 == result.status_code
